2f0754072cee810d32e8da0f61ff0365f5fc32ab
Lucas <lucas.silva1037@gmail.com>
2023-08-16 17:44:57 -0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://lucasll37.github.io/csi-26-atividade1/'
+  base: '/csi-26-atividade1/'
 })
 
 
-
+// https://lucasll37.github.io
 // ssh-keygen -t rsa -b 4096 -C "lucas.silva1037@gmail.com" -f "gh-pages" -N ""
